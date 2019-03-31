8e304cfcb7c0414c2cd8f62320ed96dc94824dff
Avital Nahon <avitaln@wix.com>
2019-03-31 15:13:53 +0300

    .

---
+++ x.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link type="image/png" href="//www.wix.com/favicon.ico" rel="shortcut icon">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+	<style>
+	html {
+	font-family: 'Roboto', sans-serif;
+	-webkit-font-smoothing: antialiased;
+}
+body {
+	margin: 0;
+}
+</style>
+</head>
+<body>
+<div id="main"></div>
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/avitaln/spot/dist/spot4.js"></script>
+	</body>
+	</html>
