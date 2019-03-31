4cb6d675bde2c4b846ee8bfd02b8674055c4559c
Avital Nahon <avitaln@wix.com>
2019-03-31 23:24:03 +0300

    .

--- ga.sh
+++ ga.sh
@@ -3,7 +3,7 @@ LASTVER=$(<latest)
 NEWVER=$(($LASTVER + 1))
 echo $NEWVER
 cd ../avitaln.github.io && ./ga.sh $LASTVER $NEWVER
-sbt fullOpsJS
+sbt fullOptJS
 cp target/scala-2.12/spot-opt.js dist/spot$NEWVER.js
 git add dist/spot$NEWVER.js
 git commit -m "GA"
