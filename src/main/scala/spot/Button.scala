97848be9952b7c321a1e5c4676f883eeb8bda987
Avital Nahon <avitaln@wix.com>
2019-04-03 01:00:33 +0300

    GA

---
+++ src/main/scala/spot/Button.scala
@@ -0,0 +1,29 @@
+package spot
+
+import com.thoughtworks.binding.{Binding, dom}
+import org.scalajs.dom.{Event, Node}
+
+object SizeButton {
+  Styles.add(
+    """
+      |.size_button {
+      |  color: black;
+      |  border: 1px solid black;
+      |  font-size: 16px;
+      |  outline: none;
+      |}
+      |.size_button:active {
+      |  opacity: 0.5;
+      |}
+      |
+    """.stripMargin
+  )
+
+  @dom def apply(orig: Dim, scaled: Dim, onClick: Event ⇒ Unit): Binding[Node] = {
+    <button class="size_button" style={sizeStyle(scaled)} onclick={ event: Event => onClick(event) }>{s"${orig.w} X ${orig.h}".toUpperCase}</button>
+  }
+
+  def sizeStyle(scaled: Dim): String = {
+    s"width:${scaled.w}px;height:${scaled.h}px"
+  }
+}
